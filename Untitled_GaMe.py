bbb850a77ab6424e77878afbdaddf16d70a1335b
carneliantius <83621688+carneliantius@users.noreply.github.com>
2021-05-06 11:12:56 -0400

    Added `Smol`.
    Had to.

--- Untitled_GaMe.py
+++ Untitled_GaMe.py
@@ -1,2 +1,2 @@
-print("Hey! This is a remake of the deleted game Untitled_GaMe by NTBeta. If you're reading this, uh you're probably a friend of mine.")
+print("Hey! This is a remake of the `smol` deleted game Untitled_GaMe by NTBeta. If you're reading this, uh you're probably a friend of mine.")
 print("Anyways, enjoy the game.")
