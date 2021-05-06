280d5878ab67b67853927d6e2295bb3507d301ad
carneliantius <83621688+carneliantius@users.noreply.github.com>
2021-05-06 11:25:44 -0400

    Added Waiting Statements.
    It won't all appear at once now, it will wait `3 seconds` before progressing in the code. Should be helpful to do this so you can actually read a sentence at a time.

--- Untitled_GaMe.py
+++ Untitled_GaMe.py
@@ -1,9 +1,18 @@
+import time
+
 print("Hey! This is a remake of the `smol` deleted game Untitled_GaMe by NTBeta. If you're reading this, uh you're probably a friend of mine.")
+time.sleep(3.0)
 print("Anyways, enjoy the game.")
+time.sleep(3.0)
 print("Welcome to the world of Untitled_GaMe, first I need to know your name.")
+time.sleep(3.0)
 print("What is it?")
+time.sleep(3.0)
 print("Your name is:")
 n = input()
+time.sleep(3.0)
 print("So your name is" + n + "? Cool.")
+time.sleep(3.0)
 print("weird noises n stuff")
+time.sleep(3.0)
 print("Ugh. That's my little brother Miko. He's a fat bumbling idiot. I plan on killing him.")
