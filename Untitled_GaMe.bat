33c00c1b0cea03c48059cb5c7096ac4bc8309bbf
Bee Mario <64273177+BeeMario@users.noreply.github.com>
2021-05-17 17:32:24 -0500

    batch port
    added it

---
+++ Untitled_GaMe.bat
@@ -0,0 +1,22 @@
+@echo off
+title Untitled_GaMe Batch Port
+cls
+echo Welcome to Untitled_GaMe/Blackhole
+echo Original game created by NTBeta
+echo Batch port created by Bee Mario
+pause
+cls
+echo Welcome to the world of Blackhole! Before you start, I need to know your name! So, what is it?
+set /p name="Answer: "
+echo Hey %Name%! You're gonna be late for class if you don't hurry!
+pause
+cls
+echo Welcome to the world of Blackhole! Before you start, I need to know your name! So, what is it?
+echo Answer: %Name%
+echo Hey %Name%! You're gonna be late for class if you don't hurry!
+echo Sigh. That loud-mouthed kid is my little brother, Miko. It's been like this for as long as I can remember. Miko telling me to hurry up after him, and me rushing to please him.
+pause
+cls
+echo The end of this version! More will be added in the future
+pause
+exit
